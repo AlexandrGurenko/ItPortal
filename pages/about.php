833ba2e213e92305314307fadf06475b38b23718
Student <student@lan.itstep.kiev.ua>
2018-11-03 11:30:25 +0200

    Заполнение страницы О сайте

--- pages/about.php
+++ pages/about.php
@@ -6,6 +6,10 @@
 
 <div class="starter-template">
     <h1>О сайте</h1>
+   	<h3>Сайт об инновациях</h3>
+   	<p>Признаюсь честно, на этом ресурсе ничего не будет, потому что он учебный и мы просто учимся работать в команде:)</p>
+   	<p>Вот Вам видео с котиками:</p>
+   	<iframe width="1280" height="720" src="https://www.youtube.com/embed/0_qvBTfhbqo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
 <?php
